aaf2e688214d3fdc50697c27342d365105b524c1
FarisAlsaif <68563462+FarisAlsaif@users.noreply.github.com>
2023-08-19 21:01:00 +0300

    Update dashboard.component.ts

--- Angular/src/app/main/dashboad/dashboard/dashboard.component.ts
+++ Angular/src/app/main/dashboad/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { HeaderService } from 'src/app/services/header/header.service';
 import { BooksService } from 'src/app/services/books/books.service';
 import { book } from '../../shared/types';
-import axios from 'axios';
 import { SearchService } from 'src/app/services/search/search.service';
 import { SearchComponent } from '../../shared/search/search.component';
 
