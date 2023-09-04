43faffd1b12a38ef20ec40e5021e3a6d94891e37
FarisAlsaif <68563462+FarisAlsaif@users.noreply.github.com>
2023-09-04 12:32:23 +0300

    Update auth-router.js

--- nodejs/routers/auth-router.js
+++ nodejs/routers/auth-router.js
@@ -12,15 +12,18 @@ router.get('/:username', async (req, res) => {
           }
     }
 );
-router.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const match = await auth.authenticateUser(username, password);
-        res.send(match);
-    } catch (error) {
-        res.status(401).json({ error: 'Authentication failed' });
-    }
-});
+// router.post('/login', async (req, res) => {
+//     try {
+//         const { username, password } = req.body;
+//         const match = await auth.authenticateUser(username, password);
+//         res.send(match);
+//     } catch (error) {
+//         res.status(401).json({ error: 'Authentication failed' });
+//     }
+// });
+router.get('/login', function(req, res, next) {
+    res.render('login');
+  });
 
 
 module.exports = router;
